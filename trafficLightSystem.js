bf23988e3721417ba991208091e0c7e80244430f
Justin Cs <138377459+thejzxt@users.noreply.github.com>
2026-05-08 23:14:21 +0530

    Initial commit

---
+++ trafficLightSystem.js
@@ -0,0 +1,69 @@
+// Smart Traffic Light System
+
+function trafficLightSystem(currentSignal, isEmergencyVehicleApproaching) {
+
+  // Check emergency vehicle
+  if (isEmergencyVehicleApproaching) {
+
+    return "IMMEDIATE GREEN";
+
+  }
+
+  // Normal traffic signal handling
+  switch (currentSignal) {
+
+    case "RED":
+      return "STOP";
+
+    case "YELLOW":
+      return "PREPARE TO STOP";
+
+    case "GREEN":
+      return "GO";
+
+    default:
+      return "INVALID SIGNAL";
+
+  }
+
+}
+
+
+// Test Cases
+
+console.log("Test Case 1:");
+console.log(
+  trafficLightSystem("RED", false)
+);
+
+console.log("------------------");
+
+
+console.log("Test Case 2:");
+console.log(
+  trafficLightSystem("YELLOW", false)
+);
+
+console.log("------------------");
+
+
+console.log("Test Case 3:");
+console.log(
+  trafficLightSystem("GREEN", false)
+);
+
+console.log("------------------");
+
+
+console.log("Test Case 4:");
+console.log(
+  trafficLightSystem("RED", true)
+);
+
+console.log("------------------");
+
+
+console.log("Test Case 5:");
+console.log(
+  trafficLightSystem("BLUE", false)
+);
